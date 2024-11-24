89be6d4173b6e26dc6fe232c2c419ac8a9a3f62f
ilmafathima <150781358+ilmafathima@users.noreply.github.com>
2024-11-24 21:06:19 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -3,24 +3,7 @@ window.addEventListener('load', () => {
     profileSection.classList.add('fade-in'); 
 });
 
-const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-
-window.onscroll = function () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
-};
-
-
-scrollToTopBtn.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
 
 
 
